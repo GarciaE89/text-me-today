c8239ecda05452377b5c0a98f825009840337dad
jondagamkd <johntaylorwoodland@gmail.com>
2021-09-02 00:06:55 -0700

    Basic html and css.  Code Ready!

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Used to have current date display on timeline, could be updated for multiple days. 
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
